ec4a96e5c5f5d75e646246eca7cf23ce13d8741d
yoshino <minakoyoshino3@gmail.com>
2022-07-13 22:29:42 +0900

    Add: ユーザー新規登録機能の追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(page_title = '')
-    base_title = 'トリコミ'
+    base_title = 'トリコミ 今、取り込み中をLINEでお知らせ'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+
   get '/terms', to: 'static_pages#terms'
   get '/privacy_policy', to: 'static_pages#privacy_policy'
   get '/contact', to: 'static_pages#contact'
+
+  resources :users, only: %i(create)
+  get '/signup', to: 'users#new'
 end
