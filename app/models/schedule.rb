6dcc0aef7907c5a34bb849b316f267860eeaa359
yoshino <minakoyoshino3@gmail.com>
2022-07-18 18:21:02 +0900

    Add: Scheduleモデルを追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,5 +4,4 @@ class Schedule < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, length: { maximum: 65535 }
   validates :start_time, presence: true
-  validates :end_time, presence: true
 end
