7fee284541f3dd1075c8c6bf6ec31373607e34da
yoshino <minakoyoshino3@gmail.com>
2022-07-20 13:33:59 +0900

    Add: settingsテーブルとSettingモデルを追加

---
+++ db/migrate/20220720042520_create_settings.rb
@@ -0,0 +1,12 @@
+class CreateSettings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :settings do |t|
+      t.belongs_to :user, index: { unique: true }, null: false, foreign_key: true
+      t.integer :message_option, null: false, default: 0
+      t.string :message, null: false, limit: 255
+      t.integer :notification_time, null: false, dafault: 0
+
+      t.timestamps
+    end
+  end
+end
