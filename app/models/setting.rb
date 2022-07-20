235db571a17f51308926f2d836a4e6f0dc1a42b9
yoshino <minakoyoshino3@gmail.com>
2022-07-20 16:45:40 +0900

    Add: settingモデルにバリデーションを追加

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,9 @@
 class Setting < ApplicationRecord
   belongs_to :user
+
+  validates :notification_time, presence: true, length: { minimum: 0, maximum: 120 }
+  validates :message_option, presence: true
+  validates :message_text, length: { maximum: 255 }
+
+  enum message_option: { only_start_time: 0, start_time_and_title: 1, all: 2 }
 end
