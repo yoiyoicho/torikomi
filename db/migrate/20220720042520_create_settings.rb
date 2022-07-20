b6a46d58898813deae460f1ecef1af11b89114d1
yoshino <minakoyoshino3@gmail.com>
2022-07-20 15:56:00 +0900

    Change: settingsテーブルの修正

--- db/migrate/20220720042520_create_settings.rb
+++ db/migrate/20220720042520_create_settings.rb
@@ -3,8 +3,8 @@ class CreateSettings < ActiveRecord::Migration[7.0]
     create_table :settings do |t|
       t.belongs_to :user, index: { unique: true }, null: false, foreign_key: true
       t.integer :message_option, null: false, default: 0
-      t.string :message, null: false, limit: 255
-      t.integer :notification_time, null: false, dafault: 0
+      t.string :message_text, limit: 255
+      t.integer :notification_time, null: false, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_042520) do
   create_table "settings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "message_option", default: 0, null: false
-    t.string "message", limit: 255, null: false
-    t.integer "notification_time", null: false
+    t.string "message_text", limit: 255
+    t.integer "notification_time", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_settings_on_user_id", unique: true
