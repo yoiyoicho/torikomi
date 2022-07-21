46718685f41a152aaf33b30548b510ce548ab071
yoshino <minakoyoshino3@gmail.com>
2022-07-21 15:38:53 +0900

    Add: Schedulesテーブルにjob_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_042520) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_060203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_042520) do
     t.datetime "end_time", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "job_id", default: "", null: false
     t.index ["user_id"], name: "index_schedules_on_user_id"
   end
 
