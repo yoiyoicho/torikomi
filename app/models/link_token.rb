c3bdf58a490fb3a7dc2536bd3f82d8a47311aa60
yoshino <minakoyoshino3@gmail.com>
2022-07-26 17:20:09 +0900

    Fix: バグを修正

--- app/models/link_token.rb
+++ app/models/link_token.rb
@@ -2,8 +2,6 @@ class LinkToken < ApplicationRecord
   belongs_to :user
   validates :token, presence: true, uniqueness: true
 
-  private
-
   def not_expired?
     created_at > 1.days.ago.in_time_zone
   end
