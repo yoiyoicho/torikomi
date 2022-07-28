a700598edb4d2ea88839ffc833855b62e3f48ac1
yoshino <minakoyoshino3@gmail.com>
2022-07-28 10:59:29 +0900

    Change: link_tokenのカラム名を変更

--- app/models/link_token.rb
+++ app/models/link_token.rb
@@ -1,6 +1,6 @@
 class LinkToken < ApplicationRecord
   belongs_to :user
-  validates :token, presence: true, uniqueness: true
+  validates :token_digest, presence: true
 
   def not_expired?
     created_at > 1.days.ago.in_time_zone
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_005353) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_015618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_005353) do
 
   create_table "link_tokens", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "token", null: false
+    t.string "token_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_link_tokens_on_user_id"
