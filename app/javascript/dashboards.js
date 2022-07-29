405170ecde3796489ce84006caaafec05d6ee50b
yoshino <minakoyoshino3@gmail.com>
2022-07-29 18:21:55 +0900

    Improve: カレンダー表示を修正

--- app/javascript/dashboards.js
+++ app/javascript/dashboards.js
@@ -4,18 +4,43 @@ Vue.use(VCalendar);
 
 new Vue({
   el: '#app',
+  mounted() {
+    const schedules_json = JSON.parse(this.$refs.schedules.dataset.schedules);
+    this.schedules = schedules_json;
+    this.setAttrs();
+  },
   data: { 
-    attrs: [
-        {
-          key: 'today',
+    schedules: {},
+    attrs: [],
+  },
+  methods: {
+    setAttrs(){
+      for (const schedule of this.schedules){
+        const schedule_id = schedule['id'];
+        const title = schedule['title'];
+        //const body = schedule['body'];
+        const start_time_date = new Date(schedule['start_time']);
+        //const end_time = schedule['end_time'];
+        //const status = schedule['status'];
+        this.attrs.push({
+          key: `schedule-${schedule_id.toString()}`,
+          bar: {
+            height: '3px',
+            backgroundColor: 'red',
+            borderColor: null,
+            borderWidth: '1px',
+            borderStyle:'solid',
+            opacity: 1
+          },
           highlight: {
             backgroundColor: '#ff8080',
           },
-          dates: new Date(),
+          dates: start_time_date,
           popover: {
-            label: 'メッセージを表示できます',
+            label: start_time_date.getHours().toString() + ':' + start_time_date.getMinutes().toString() + ' ' + title
           },
-        }
-    ],
+        });
+      }
+    }
   }
 });
