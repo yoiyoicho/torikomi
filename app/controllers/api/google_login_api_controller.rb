4d63c726c7be0e105fa737e7a601e2ad0e038ada
yoshino <minakoyoshino3@gmail.com>
2022-07-30 17:48:51 +0900

    Add: Googleログイン後の処理を追加

--- app/controllers/api/google_login_api_controller.rb
+++ app/controllers/api/google_login_api_controller.rb
@@ -1,9 +1,14 @@
 class Api::GoogleLoginApiController < ApplicationController
+  require "googleauth/id_tokens/errors"
+  require "googleauth/id_tokens/verifier"
+
   skip_before_action :require_login
   protect_from_forgery except: :callback
 
   def callback
-    binding.pry
+    payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV['GOOGLE_CLIENT_ID'])
+    # email = payload['email']
+    # name = payload['name']
     redirect_to dashboards_path, success: 'ログインしました'
   end
 end
