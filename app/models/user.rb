80f84dca5ed6453929f92cbaf60f321d449969d9
yoshino <minakoyoshino3@gmail.com>
2022-07-30 21:46:13 +0900

    Add: usersテーブルにlogin_typeカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, uniqueness: true, presence: true
+  validates :email, presence: true, uniqueness: { scope: :type }
+
+  enum type: { default: 0, google: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_015618) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_085000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_015618) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.integer "login_type", default: 0, null: false
   end
 
   add_foreign_key "link_tokens", "users"
