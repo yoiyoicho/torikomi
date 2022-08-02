8f9a6af06b76435603275bac763251155b5be86d
yoshino <minakoyoshino3@gmail.com>
2022-08-02 16:58:04 +0900

    Fix: sidekiqの設定を本番用に変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
