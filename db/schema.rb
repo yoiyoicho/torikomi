da1fd2780cbed6a5ab2d5297c08b54bb12ce0df2
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-18 23:46:47 +0900

    Add: DBにパスワードリセット関連カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_141354) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_144239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_141354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "login_type", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "google_calendar_settings", "users"
