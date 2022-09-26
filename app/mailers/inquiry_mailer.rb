6672ed4a99280610d28bd3e4372bc82cc5a66286
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-26 15:27:32 +0900

    Add: お問い合わせの控えメールを送る処理を追加

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,6 +2,6 @@ class InquiryMailer < ApplicationMailer
   def inquiry_email(email, body)
     @email = email #返信先メールアドレス
     @body = body #お問い合わせ内容
-    mail to: ENV['GMAIL_ADDRESS'], subject: "お問い合わせ受付のお知らせ（トリコミ）"
+    mail to: [email, ENV['GMAIL_ADDRESS']], subject: "お問い合わせ受付のお知らせ（トリコミ）"
   end
 end
