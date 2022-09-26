e704e49a68535f4893b56f4c6e30ca099baecd28
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-26 14:57:36 +0900

    Fix: Inquiryモデルの修正

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,10 @@
-class Inquiry < ApplicationRecord
-  validates :body, presence: true, length: { maximum: 65535 }
+class Inquiry
+  # DBと結びつかない（＝ActiveRecordを継承しない）クラスでActive Modelの機能を使う
+  # https://railsguides.jp/active_model_basics.html
+  include ActiveModel::Model
+
+  attr_accessor :email, :body
+
   validates :email, presence: true
+  validates :body, presence: true, length: { maximum: 65535 }
 end
