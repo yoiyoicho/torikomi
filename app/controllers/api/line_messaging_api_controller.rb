2053d8332b339ccf125f6026d2a58432d5eb6337
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-28 16:06:00 +0900

    Improve: 不要なコードを削除

--- app/controllers/api/line_messaging_api_controller.rb
+++ app/controllers/api/line_messaging_api_controller.rb
@@ -3,59 +3,7 @@ class Api::LineMessagingApiController < ApplicationController
   require 'json'
   require 'typhoeus'
 
-  # 外部からのPOSTリクエストを受けるためにCSRFトークンの検証を制御する
-  # protect_from_forgery with: :null_session
   skip_before_action :require_login
-
-  # def callback
-  #   body = request.body.read
-  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  #   unless client.validate_signature(body, signature)
-  #     header_only(:bad_request)
-  #   end
-
-  #   events = client.parse_events_from(body)
-
-  #   events.each do |event|
-  #     case event
-  #     when Line::Bot::Event::Message
-  #       case event.type
-  #       when Line::Bot::Event::MessageType::Text
-  #         line_user = LineUser.new
-
-  #         # LINEユーザーのuserIDを取得
-  #         line_user_id = event['source']['userId']
-  #         line_user.assign_attributes(line_user_id: line_user_id)
-
-  #         # LINEユーザーのプロフィールを取得
-  #         profile = get_line_user_profile(line_user_id)
-  #         line_user.assign_attributes(profile)
-
-  #         # LINEユーザーから送られたワンタイムパスワードから、対応するトリコミのユーザーを見つける
-  #         otp = event.message['text']
-  #         hotp = ROTP::HOTP.new(ENV['OTP_SECRET'])
-  #         User.all.each do |user|
-  #           if hotp.verify(otp, user.id)
-  #             line_user.user_id = user.id
-  #           end
-  #         end
-
-  #         # LINEユーザーをデータベースに保存する
-  #         if line_user.save
-  #           response_text = '成功しました'
-  #         else
-  #           response_text = '失敗しました'
-  #         end
-  #         message = {
-  #           type: 'text',
-  #           text: response_text
-  #         }
-  #         client.push_message(line_user_id, message)
-  #       end
-  #     end
-  #   end
-  #   head :ok
-  # end
 
   def send_broadcast_message(text)
     message = {
@@ -86,22 +34,4 @@ class Api::LineMessagingApiController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
-
-  # def get_line_user_profile(line_user_id)
-  #   url = "https://api.line.me/v2/bot/profile/:line_user_id".gsub(':line_user_id', line_user_id)
-  #   options = {
-  #     method: 'get',
-  #     headers: {
-  #       "Authorization" => "Bearer #{ENV['LINE_CHANNEL_TOKEN']}"
-  #     },
-  #   }
-  #   request = Typhoeus::Request.new(url, options)
-  #   response = request.run
-  #   
-  #   if response.code == 200
-  #     { display_name: JSON.parse(response.body)['displayName'], picture_url: JSON.parse(response.body)['pictureUrl']||=nil }
-  #   else
-  #     {}
-  #   end
-  # end
 end
