3e9db7d115a2ef2048430bd962266777d87ac444
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-29 13:51:42 +0900

    Fix: 日本語化ファイルを修正

--- app/controllers/google_calendar_tokens_controller.rb
+++ app/controllers/google_calendar_tokens_controller.rb
@@ -2,6 +2,6 @@ class GoogleCalendarTokensController < ApplicationController
   def destroy
     google_calendar_token = current_user.google_calendar_token
     google_calendar_token.destroy!
-    redirect_back_or_to dashboards_path, success: 'Googleカレンダーとの連携を解除しました'
+    redirect_back_or_to dashboards_path, success: t('.success')
   end
 end
