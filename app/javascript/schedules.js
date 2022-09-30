cf2cd8694a59b152a3bfed98ff71233da8cf7369
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-30 11:22:09 +0900

    Fix: Vueの動作条件を修正

--- app/javascript/schedules.js
+++ app/javascript/schedules.js
@@ -1,37 +1,39 @@
 import Vue from 'vue/dist/vue.esm.js';
 
-new Vue({
-  el: '#schedule-list',
-  data(){
-    return{
-      toBeSent: true,
-      sent: false
-    };
-  },
-  methods:{
-    toBeSentTabClick(){
-      this.toBeSent = true;
-      this.sent = false;
-    },
-    sentTabClick(){
-      this.toBeSent = false;
-      this.sent = true;
-    }
-  },
-  computed: {
-    toBeSentStyle(){
-      if(this.toBeSent == true){
-        return ''
-      }else{
-        return 'display: none;'
+if(document.querySelector('#schedule-list')){
+  new Vue({
+    el: '#schedule-list',
+    data(){
+      return{
+        toBeSent: true,
+        sent: false
       };
     },
-    sentStyle(){
-      if(this.sent == true){
-        return ''
-      }else{
-        return 'display: none;'
-      };
+    methods:{
+      toBeSentTabClick(){
+        this.toBeSent = true;
+        this.sent = false;
+      },
+      sentTabClick(){
+        this.toBeSent = false;
+        this.sent = true;
+      }
+    },
+    computed: {
+      toBeSentStyle(){
+        if(this.toBeSent == true){
+          return ''
+        }else{
+          return 'display: none;'
+        };
+      },
+      sentStyle(){
+        if(this.sent == true){
+          return ''
+        }else{
+          return 'display: none;'
+        };
+      }
     }
-  }
-});
+  });
+}
