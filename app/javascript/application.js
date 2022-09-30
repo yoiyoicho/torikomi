cd27747971e1fc708f89ff68eb010d4b4161a04c
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-09-30 11:23:16 +0900

    Add: Turboを全体で無効にする設定を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
-import "@hotwired/turbo-rails"
-
+import "@hotwired/turbo-rails";
 import './dashboards.js';
 import './schedules.js';
+
+Turbo.session.drive = false;
 
 /*const app = new Vue({
   el: '#app',
