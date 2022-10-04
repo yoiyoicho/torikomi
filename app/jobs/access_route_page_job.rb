1675907b96a5fac3887898441a9b88ac23e107ce
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-04 11:41:47 +0900

    Add: ジョブを修正

--- app/jobs/access_route_page_job.rb
+++ app/jobs/access_route_page_job.rb
@@ -1,7 +1,10 @@
 class AccessRoutePageJob < ApplicationJob
   require 'typhoeus'
+  queue_as :default
+
   def perform
     url = 'https://torikomi.herokuapp.com'
     response = Typhoeus::Request.get(url)
   end
+
 end
--- app/jobs/delete_old_link_tokens_job.rb
+++ app/jobs/delete_old_link_tokens_job.rb
@@ -1,7 +1,11 @@
 class DeleteOldLinkTokensJob < ApplicationJob
+
+  queue_as :default
+
   def perform
     LinkToken.where("created_at < ?", 1.days.ago.in_time_zone).each do |old_link_token|
       old_link_token.destroy!
     end
   end
+
 end
