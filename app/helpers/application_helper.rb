f50f39d7db2008af63872632373d851308dbda0e
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-18 12:33:46 +0900

    Change: ページタイトルを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(page_title = '')
-    base_title = 'トリコミ - 今、取り込み中をLINEでお知らせ'
+    base_title = 'トリコミ - リモートワーカー向けスケジュール共有サービス'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
 end
