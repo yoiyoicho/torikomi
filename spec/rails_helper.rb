bb0f4a1a65bbd52538ccf4225ebd1745809772d1
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-25 10:55:06 +0900

    Add: FactoryBotの設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotを使用する
+  config.include FactoryBot::Syntax::Methods
 end
