5a4482ddc7da5aab926f22d462b715c05d57af68
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-25 10:55:44 +0900

    Add: Userのモデルテストを追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'バリデーション' do
+
+    context 'デフォルトユーザー' do
+      it '正常系' do
+        user = build(:user, :default)
+        expect(user).to be_valid
+        expect(user.errors).to be_empty
+      end
+      it '異常系：emailがない' do
+        user_without_email = build(:user, :default, email: '')
+        expect(user_without_email).to be_invalid
+        expect(user_without_email.errors[:email]).to eq ['を入力してください']
+      end
+      it '異常系：passwordがない' do
+        user_without_password = build(:user, :default, password: '')
+        expect(user_without_password).to be_invalid
+        expect(user_without_password.errors[:password]).to eq ['は3文字以上で入力してください']
+      end
+      it '異常系：password_confirmationがない' do
+        user_without_password_confirmation = build(:user, :default, password_confirmation: '')
+        expect(user_without_password_confirmation).to be_invalid
+        expect(user_without_password_confirmation.errors[:password_confirmation]).to eq ['とパスワードの入力が一致しません', 'を入力してください']
+      end
+      it '異常系：passwordとpassword_confirmationが異なる' do
+        user_with_different_passwords = build(:user, :default, password: 'password', password_confirmation: 'password_confirmation')
+        expect(user_with_different_passwords).to be_invalid
+        expect(user_with_different_passwords.errors[:password_confirmation]).to eq ['とパスワードの入力が一致しません']
+      end
+      it '異常系：passwordが3文字未満' do
+        user_with_short_password = build(:user, :default, password: 'pa', password_confirmation: 'pa')
+        expect(user_with_short_password).to be_invalid
+        expect(user_with_short_password.errors[:password]).to eq ['は3文字以上で入力してください']
+      end
+      it '異常系：email（デフォルトログイン）が既に存在する' do
+        existing_user = create(:user, :default)
+        new_user = build(:user, :default, email: existing_user.email)
+        expect(new_user).to be_invalid
+        expect(new_user.errors[:email]).to eq ['はすでに存在します']
+      end
+      it '正常系：email（Googleログイン）が既に存在する' do
+        existing_google_login_user = create(:user, :google)
+        new_user = build(:user, :default, email: existing_google_login_user.email)
+        expect(new_user).to be_valid
+        expect(new_user.errors).to be_empty
+      end
+    end
+
+    context 'Googleログインユーザー' do
+      before { create(:user, :google) }
+      it '正常系' do
+        user = build(:user, :google)
+        expect(user).to be_valid
+        expect(user.errors).to be_empty
+      end
+      it '正常系：email（デフォルトログイン）が既に存在する' do
+        existing_user = create(:user, :default)
+        new_user = build(:user, :google, email: existing_user.email)
+        expect(new_user).to be_valid
+        expect(new_user.errors).to be_empty
+      end
+      it '異常系：email（Googleログイン）が既に存在する' do
+        existing_google_login_user = create(:user, :google)
+        new_user = build(:user, :google, email: existing_google_login_user.email)
+        expect(new_user).to be_invalid
+        expect(new_user.errors[:email]).to eq ['はすでに存在します']
+      end
+    end
+  end
+end
