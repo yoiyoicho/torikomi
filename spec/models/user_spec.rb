eafa6909eb9ad50a98f361ce9ac1277f8b002a7f
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-26 16:07:03 +0900

    Improve: Userのモデルテストを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーション' do
-
     context 'デフォルトユーザー' do
       it '正常系' do
         user = build(:user, :default)
@@ -12,33 +11,33 @@ RSpec.describe User, type: :model do
       it '異常系：emailがない' do
         user_without_email = build(:user, :default, email: '')
         expect(user_without_email).to be_invalid
-        expect(user_without_email.errors[:email]).to eq ['を入力してください']
+        expect(user_without_email.errors[:email]).to eq [I18n.t('errors.messages.blank')]
       end
       it '異常系：passwordがない' do
         user_without_password = build(:user, :default, password: '')
         expect(user_without_password).to be_invalid
-        expect(user_without_password.errors[:password]).to eq ['は3文字以上で入力してください']
+        expect(user_without_password.errors[:password]).to eq [I18n.t('errors.messages.too_short', count: 3)]
       end
       it '異常系：password_confirmationがない' do
         user_without_password_confirmation = build(:user, :default, password_confirmation: '')
         expect(user_without_password_confirmation).to be_invalid
-        expect(user_without_password_confirmation.errors[:password_confirmation]).to eq ['とパスワードの入力が一致しません', 'を入力してください']
+        expect(user_without_password_confirmation.errors[:password_confirmation]).to eq [I18n.t('errors.messages.confirmation', attribute: User.human_attribute_name(:password)),I18n.t('errors.messages.blank')]
       end
       it '異常系：passwordとpassword_confirmationが異なる' do
         user_with_different_passwords = build(:user, :default, password: 'password', password_confirmation: 'password_confirmation')
         expect(user_with_different_passwords).to be_invalid
-        expect(user_with_different_passwords.errors[:password_confirmation]).to eq ['とパスワードの入力が一致しません']
+        expect(user_with_different_passwords.errors[:password_confirmation]).to eq [I18n.t('errors.messages.confirmation', attribute: User.human_attribute_name(:password))]
       end
       it '異常系：passwordが3文字未満' do
         user_with_short_password = build(:user, :default, password: 'pa', password_confirmation: 'pa')
         expect(user_with_short_password).to be_invalid
-        expect(user_with_short_password.errors[:password]).to eq ['は3文字以上で入力してください']
+        expect(user_with_short_password.errors[:password]).to eq [I18n.t('errors.messages.too_short', count: 3)]
       end
       it '異常系：email（デフォルトログイン）が既に存在する' do
         existing_user = create(:user, :default)
         new_user = build(:user, :default, email: existing_user.email)
         expect(new_user).to be_invalid
-        expect(new_user.errors[:email]).to eq ['はすでに存在します']
+        expect(new_user.errors[:email]).to eq [I18n.t('errors.messages.taken')]
       end
       it '正常系：email（Googleログイン）が既に存在する' do
         existing_google_login_user = create(:user, :google)
@@ -65,7 +64,7 @@ RSpec.describe User, type: :model do
         existing_google_login_user = create(:user, :google)
         new_user = build(:user, :google, email: existing_google_login_user.email)
         expect(new_user).to be_invalid
-        expect(new_user.errors[:email]).to eq ['はすでに存在します']
+        expect(new_user.errors[:email]).to eq [I18n.t('errors.messages.taken')]
       end
     end
   end
