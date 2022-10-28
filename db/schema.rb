30754735e4cbd29227e508021ab0158812f9bd61
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-10-28 15:42:15 +0900

    Add: usersテーブルにroleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_083514) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_063524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_083514) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
+    t.integer "role", default: 0, null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
