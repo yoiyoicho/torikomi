e12525a93b0fa46fa5e5b22e923387626f746dbd
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2023-02-07 14:45:31 +0900

    Fix: バグ修正

--- app/jobs/send_line_message_job.rb
+++ app/jobs/send_line_message_job.rb
@@ -37,7 +37,7 @@ class SendLineMessageJob < ApplicationJob
     message = "スケジュールをお知らせします！" + "\n"
 
     if setting.only_time?
-      message << "\n" + "日時：" + sschedule.start_time.strftime('%Y年%m月%d日 %H時%M分')
+      message << "\n" + "日時：" + schedule.start_time.strftime('%Y年%m月%d日 %H時%M分')
     elsif setting.time_and_title?
       message << "\n" + "日時：" + schedule.start_time.strftime('%Y年%m月%d日 %H時%M分')
       message << "\n" + "タイトル：" + schedule.title
