8339c83def41d4dc5e00553fe5be9bf755182599
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2023-08-06 22:43:16 +0900

    Magiaの設定削除

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# 自分のアプリに合わせて必要なコマンドを修正してください
-bin/rails db:migrate
-
-rm -f tmp/pids/server.pid && bin/rails s
